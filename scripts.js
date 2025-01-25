010341b9272f8eb15da2819b0f055afd5ac16e44
kysch13 <124356972+kysch13@users.noreply.github.com>
2025-01-24 21:41:36 -0500

    Add loader and error displays

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ const elements = (function() {
     const body = document.querySelector('body');
     const searchCont = document.getElementById('search-cont');
     const searchInput = document.getElementById('search-bar');
+    const loader = document.querySelector('.loader');
+    const errorMsg = document.getElementById('error-msg');
     const forecastBox = document.getElementById('forecast-box');
     const untisToggle = document.getElementById('units-toggle');
     const sevenDayForecast = document.getElementById('sevenday-forecast-card-cont');
@@ -10,7 +12,7 @@ const elements = (function() {
     const currentCondMain = document.querySelector('.current-conditions-main');
     const currentCondSecondary = document.querySelector('.current-conditions-secondary');
 
-    return {searchCont, searchInput, forecastBox, untisToggle, sevenDayForecast, currentWeatherCard, locationHeader, currentCondMain, currentCondSecondary, body}
+    return {searchCont, searchInput, loader, errorMsg, forecastBox, untisToggle, sevenDayForecast, currentWeatherCard, locationHeader, currentCondMain, currentCondSecondary, body}
 })();
 
 const createIcon = function (icon) {
@@ -41,11 +43,18 @@ const requestData = async function() {
             populateDashboard();
         }
         if (response.status === 400) {
-            console.log('No such location found');
+            displayErrorMsg(`Sorry, no results were found for your location.`);
         }
     } catch (error) {
         console.error(error);
+        displayErrorMsg(`Something's gone wrong. Please try again later.`);
     }
+}
+
+function displayErrorMsg(msg){
+    clearDashboard();
+    elements.errorMsg.innerText = msg;
+    elements.errorMsg.classList.add('active');
 }
 
 function getWeekDay(date) {
@@ -58,6 +67,13 @@ function clearDashboard() {
     elements.locationHeader.innerHTML = '';
     elements.currentCondMain.innerHTML = '';
     elements.currentCondSecondary.innerHTML = '';
+    elements.loader.classList.remove('active');
+    elements.errorMsg.classList.remove('active');
+}
+
+function displayLoader() {
+    clearDashboard();
+    elements.loader.classList.add('active');
 }
 
 const populateDashboard = function() {
@@ -186,6 +202,7 @@ elements.searchCont.addEventListener('keydown', (e) => {
     if (e.key == 'Enter') {
         e.preventDefault();
         weatherQuery.location = elements.searchInput.value.toString();
+        displayLoader();
         requestData();
     }
 })
@@ -196,6 +213,7 @@ elements.untisToggle.addEventListener('click', (e) => {
     } else {
         weatherQuery.units = 'metric';
     }
+    displayLoader();
     requestData();
     
 })
@@ -203,9 +221,11 @@ elements.untisToggle.addEventListener('click', (e) => {
 /* GeoLocation */
 async function getGeoLocation() {
     if ("geolocation" in navigator) {
+        displayLoader();
         const location = await navigator.geolocation.getCurrentPosition((position) => {
-             lat = position.coords.latitude;
-             lng = position.coords.longitude;
+             weatherQuery.lat = position.coords.latitude;
+             weatherQuery.lng = position.coords.longitude;
+             reverseGeocode();
         });
     } else {
         return null;
@@ -213,17 +233,18 @@ async function getGeoLocation() {
       
 }
 
-async function reverseGeocode(lat, lng) {
+async function reverseGeocode() {
     const apiKey = 'EYUEBegUWE8HeKPoc2QXeeK7AkWTv5mi';
     try {
-        const response = await fetch(`https://api.geocodify.com/v2/reverse?api_key=${apiKey}&lat=${lat}&lng=${lng}`, {mode: 'cors'});
+        const response = await fetch(`https://api.geocodify.com/v2/reverse?api_key=${apiKey}&lat=${weatherQuery.lat}&lng=${weatherQuery.lng}`, {mode: 'cors'});
         if (response.status === 200) {
             const responseData = await response.json();
             const locality = responseData.response.features[0].properties.locality;
             const region = responseData.response.features[0].properties.region;
             const country = responseData.response.features[0].properties.country;
             const locationName = `${locality}, ${region}, ${country}`;
-            weatherQuery.location = locationName;   
+            weatherQuery.location = locationName;
+            requestData();
         }
     } catch(error) {
         console.error(error);
@@ -231,10 +252,5 @@ async function reverseGeocode(lat, lng) {
 
 }
 
-/*
-const searchGeo = (async function() {
-    getGeoLocation();
-
-})();
-*/
- getGeoLocation();
+// Try getting geo location
+getGeoLocation();
